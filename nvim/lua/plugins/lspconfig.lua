c627975ea16c7b3f2e6d4ad450c7a21ae81a128c
marcos-x0 <56_despots_parsnip@icloud.com>
2024-10-25 12:05:44 -0300

    fixed lspconfig

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,16 +1,29 @@
 return {
   -- Main LSP Configuration
   "neovim/nvim-lspconfig",
-  config = function()
-    local nvim_lsp = require("lspconfig")
-    nvim_lsp.nil_ls.setup({
-      settings = {
+  opts = function(_, opts)
+    -- Check if nil_ls is part of the server configurations
+    if opts.servers and opts.servers.nil_ls then
+      -- Extend the existing configuration for nil_ls
+      opts.servers.nil_ls.settings = vim.tbl_deep_extend("force", opts.servers.nil_ls.settings or {}, {
         ["nil"] = {
           formatting = {
             command = { "nixfmt" },
           },
         },
-      },
-    })
+      })
+    else
+      -- If nil_ls is not configured, you can add it
+      opts.servers = opts.servers or {}
+      opts.servers.nil_ls = {
+        settings = {
+          ["nil"] = {
+            formatting = {
+              command = { "nixfmt" },
+            },
+          },
+        },
+      }
+    end
   end,
 }
