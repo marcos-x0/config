8066ac27903af86636418bbd7a0f66fb42747912
marcox-x0 <56_despots_parsnip@icloud.com>
2024-12-26 19:13:54 -0300

    added ghostty. but still have problem. not useable

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/noice.nvim",
+  opts = function(_, opts)
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = "*",
+      callback = function()
+        local filename = vim.fn.expand("%") -- Get the current file's name
+        require("noice").notify(string.format("%s", filename), "info", { title = "Saved" })
+      end,
+    })
+  end,
+}
