eaf09587b62f9af6c02c6630c5ab7bff9ccd1f93
marcox-x0 <56_despots_parsnip@icloud.com>
2025-01-11 14:21:43 -0300

    removed caret plugin

--- nvim/lua/plugins/caret.lua
+++ nvim/lua/plugins/caret.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "caret.nvim",
-    dir = "~/.work/caret.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    config = function()
-      require("caret").setup()
-    end,
-    keys = {
-      {
-        "<leader>aa",
-        function()
-          require("caret").open_sidebar()
-        end,
-        desc = "Open Caret.nvim sidebar",
-      },
-    },
-  },
+  -- {
+  --   "caret.nvim",
+  --   dir = "~/.work/caret.nvim",
+  --   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  --   config = function()
+  --     require("caret").setup()
+  --   end,
+  --   keys = {
+  --     {
+  --       "<leader>aa",
+  --       function()
+  --         require("caret").open_sidebar()
+  --       end,
+  --       desc = "Open Caret.nvim sidebar",
+  --     },
+  --   },
+  -- },
 }
