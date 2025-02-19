3f650759d9516f7e6660bf0db415b501a8df1677
marcox-x0 <56_despots_parsnip@icloud.com>
2025-02-19 12:05:30 -0300

    updated lazyvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,6 +6,13 @@ vim.opt.colorcolumn = "80"
 vim.opt.textwidth = 79
 vim.g.snacks_animate = false
 
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Enable spell check
+vim.opt.spell = true
+vim.opt.spelllang = { "en_us" }
 -- vim.opt.formatoptions:append("w") -- Break lines at whitespace
 -- vim.opt.linebreak = true
 -- vim.opt.wrap = true --vim.opt.formatoptions:append("l") -- Break long words
