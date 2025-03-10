510c605f73058ae2a6a75362e56db19c1559c1d3
marcox-x0 <56_despots_parsnip@icloud.com>
2025-03-10 15:30:16 -0300

    improved fish and starship

---
+++ fish/conf.d/bind_bang_dollar.fish
@@ -0,0 +1,24 @@
+# the equivalent of bash's !$ and !! in the fish shell
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
---
+++ fish/conf.d/setup_paths.fish
@@ -0,0 +1,10 @@
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.cargo/bin/
+fish_add_path /run/current-system/sw/bin
+fish_add_path /usr/local/bin
+fish_add_path /usr/bin
+fish_add_path /usr/sbin
+fish_add_path /bin
+fish_add_path /sbin
+# fish_add_path $HOME/.local/share/containers/podman-desktop/extensions-storage/compose/bin
+# fish_add_path $HOME/.local/share/containers/podman-desktop/extensions-storage/kind/kind-darwin-arm64
